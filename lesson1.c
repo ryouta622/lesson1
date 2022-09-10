3a4cb9a19755fd3db8ac8d81723c23bb48276364
ryouta622 <trip3art@gmail.com>
2022-09-11 03:23:43 +0900

    test commit

--- lesson1.c
+++ lesson1.c
@@ -1,7 +1,13 @@
 #include <strting.h>
 
+int add(int a,int b);
+
 int Subject(int a){
     int value;
-  value = a * 5;
+	value = add(3,9);
   return  value;
 }
+
+int add(int a,int b){
+	return a+b;
+}	
